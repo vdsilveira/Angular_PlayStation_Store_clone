a9042e76ddf1e60533c78afff251cb3f889e942e
vdsilveira <vdsilveira@gmail.com>
2023-07-10 21:56:25 -0300

    content created

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input()
+  gamecover:string=""
+
+  @Input()
+gamelabel:string=""
+
+@Input()
+gametype:string=""
+@Input()
+gameprice:string=""
+
+}
